1f8897ff3cd64baf208ec9300567053188e1a69f
aliaksandrYan <aliaksandr.yan@yandex.by>
2019-07-17 12:14:29 +0300

    adding login page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,20 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage(object):
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout = 5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
     def open(self):
     	self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
+    def go_to_login_page(self):
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
